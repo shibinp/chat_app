16cfa403909b93428a13f6bd820af7d8c52d6b2c
shibinp <shibinpmpm@gamil.com>
2018-09-26 23:33:25 +0530

    devise gem added for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  #esources :messages
-  #post "login" =>  "messages#login"
-  #@ot "messages#welcome"
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
